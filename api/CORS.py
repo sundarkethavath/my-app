24e5cbfa558eb537806a8fc31307507144213124
sundarkethavath <kethavath.sundar@gramener.com>
2025-01-29 14:56:46 +0530

    Update CORS.py

--- api/CORS.py
+++ api/CORS.py
@@ -1,4 +1,7 @@
 def enable_cors(handler):
     handler.send_header('Access-Control-Allow-Origin', '*')
-    handler.send_header('Access-Control-Allow-Methods', 'GET')
+    handler.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')  # Allow OPTIONS for pre-flight
     handler.send_header('Access-Control-Allow-Headers', 'Content-Type')
+    if handler.command == 'OPTIONS':
+        handler.send_response(200)
+        handler.end_headers()
