01615eb0246b1ca892e9344ea2209af05374780e
sundarkethavath <kethavath.sundar@gramener.com>
2025-01-29 16:45:51 +0530

    Delete api/CORS.py

--- api/CORS.py
+++
@@ -1,7 +0,0 @@
-def enable_cors(handler):
-    handler.send_header('Access-Control-Allow-Origin', '*')
-    handler.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')  # Allow OPTIONS for pre-flight
-    handler.send_header('Access-Control-Allow-Headers', 'Content-Type')
-    if handler.command == 'OPTIONS':
-        handler.send_response(200)
-        handler.end_headers()
